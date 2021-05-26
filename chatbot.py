74d2953deb04883adbd5ab9b06ea2a688337f05f
JUSTICE OHENE AMOFA <70078600+Joamofa1@users.noreply.github.com>
2021-05-26 15:03:17 +0000

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -32,7 +32,6 @@ send_message("hello")
 
 
 CHIT CHAT 1
-CHIT CHAT 
 # Define variables
 name = "Greg"
 weather = "cloudy"
@@ -78,3 +77,38 @@ def respond(message):
         # Return the "default" message
         bot_message = responses["default"]
     return bot_message
+  
+  ADDING VARIETY 
+  
+  # Import the random module
+import random 
+
+name = "Greg"
+weather = "cloudy"
+
+# Define a dictionary containing a list of responses for each message
+responses = {
+  "what's your name?": [
+      "my name is {0}".format(name),
+      "they call me {0}".format(name),
+      "I go by {0}".format(name)
+   ],
+  "what's today's weather?": [
+      "the weather is {0}".format(weather),
+      "it's {0} today".format(weather)
+    ],
+  "default": ["default message"]
+}
+
+# Use random.choice() to choose a matching response
+def respond(message):
+    # Check if the message is in the responses
+    if message in responses:
+        # Return a random matching response
+        bot_message = random.choice(responses[message])
+    else:
+        # Return a random "default" response
+        bot_message = random.choice(responses["default"])
+    return bot_message
+
+  ELIZA I: asking questions
