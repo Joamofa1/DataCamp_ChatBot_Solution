0436393f2f53a0003359d4b61b1c01a0d50c4b30
JUSTICE OHENE AMOFA <70078600+Joamofa1@users.noreply.github.com>
2021-05-26 14:42:14 +0000

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,80 @@
+  BOT 1 
+    
+    bot_template = "BOT : {0}"
+user_template = "USER : {0}"
+
+# Define a function that responds to a user's message: respond
+def respond(bot_message):
+    # Concatenate the user's message to the end of a standard bot respone
+    bot_message = "I cant hear you! You said:"+ bot_message
+    # Return the result
+    return bot_message
+# Test function
+print(respond("hello!"))
+
+BOT 2  
+
+# Create templates
+bot_template = "BOT : {0}"
+user_template = "USER : {0}"
+
+# Define a function that sends a message to the bot: send_message
+def send_message(message):
+    # Print user_template including the user_message
+    print(user_template.format(message))
+    # Get the bot's response to the message
+    response = respond(message)
+    # Print the bot template including the bot's response.
+    print(bot_template.format(response))
+
+# Send a message to the bot
+send_message("hello")
+
+
+CHIT CHAT 1
+CHIT CHAT 
+# Define variables
+name = "Greg"
+weather = "cloudy"
+
+# Define a dictionary with the predefined responses
+responses = {
+  "what's your name?": "my name is {0}".format(name),
+  "what's today's weather?": "the weather is {0}".format(weather),
+  "default": "default message"
+}
+
+# Return the matching response if there is one, default otherwise
+def respond(message):
+    # Check if the message is in the responses
+    if message in responses:
+        # Return the matching message
+        bot_message = responses[message]
+    else:
+        # Return the "default" message
+        bot_message = responses["default"]
+    return bot_message
+    
+    
+    CHIT CHAT 2
+    # Define variables
+name = "Greg"
+weather = "cloudy"
+
+# Define a dictionary with the predefined responses
+responses = {
+  "what's your name?": "my name is {0}".format(name),
+  "what's today's weather?": "the weather is {0}".format(weather),
+  "default": "default message"
+}
+
+# Return the matching response if there is one, default otherwise
+def respond(message):
+    # Check if the message is in the responses
+    if message in responses:
+        # Return the matching message
+        bot_message = responses[message]
+    else:
+        # Return the "default" message
+        bot_message = responses["default"]
+    return bot_message
